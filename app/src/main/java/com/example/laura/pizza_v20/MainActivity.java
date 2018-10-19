0a615e44aeee1545c1b0807c17f35d9ec3e8f8ed
LauraEchis <laura.echis@gmail.com>
2018-10-19 10:16:59 +0400

    -- I HATE STRINGS IN JAVA buttons word but those strings it doesn't want to import why WHY WHY, MISTER ANDERSON, WHY?

--- app/src/main/java/com/example/laura/pizza_v20/MainActivity.java
+++ app/src/main/java/com/example/laura/pizza_v20/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.laura.pizza_v20;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,9 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static com.example.laura.pizza_v20.R.*;
+import static com.example.laura.pizza_v20.R.string.*;
 
 public class MainActivity extends AppCompatActivity {
     TextView cardNumber;
@@ -38,30 +42,30 @@ public class MainActivity extends AppCompatActivity {
     int pineapple = 60;
     int cheese = 25;
     int rad25 = 200;
-    int rad30 = 0;
-    int rad35 = 0;
+    int rad30 = 300;
+    int rad35 = 350;
     int previous_cost = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        cardNumber = findViewById(R.id.cardNumber);
-        cardCCV = findViewById(R.id.cardCVV);
-        cardDate = findViewById(R.id.dateCardEnd);
-        full_amount = findViewById(R.id.txtResult);
+        setContentView(layout.activity_main);
+        cardNumber = findViewById(id.cardNumber);
+        cardCCV = findViewById(id.cardCVV);
+        cardDate = findViewById(id.dateCardEnd);
+        full_amount = findViewById(id.txtResult);
         full_amount.setText(Integer.toString(0));
-        spinner = findViewById(R.id.spinner_pay);
+        spinner = findViewById(id.spinner_pay);
         initialValue = spinner.getSelectedItem().toString();
         spinner.setOnItemSelectedListener(new SpinnerSelectedListener(initialValue));
         cardDate.setVisibility(View.VISIBLE);
         cardNumber.setVisibility(View.VISIBLE);
         cardCCV.setVisibility(View.VISIBLE);
-        rb = findViewById(R.id.radioButton);
-        rb2 = findViewById(R.id.radioButton2);
-        rb3 = findViewById(R.id.radioButton3);
+        rb = findViewById(id.radioButton);
+        rb2 = findViewById(id.radioButton2);
+        rb3 = findViewById(id.radioButton3);
         rb.setChecked(true);
-        full_amount = findViewById(R.id.txtResult);
+        full_amount = findViewById(id.txtResult);
         int all_score = Integer.parseInt((full_amount.getText().toString()));
         all_score = all_score + rad25 - previous_cost;
         previous_cost = rad25;
@@ -112,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
         rb2.setEnabled(true);
         rb3.setEnabled(true);
         rb.setEnabled(false);
-        full_amount = findViewById(R.id.txtResult);
+        full_amount = findViewById(id.txtResult);
         int all_score = Integer.parseInt((full_amount.getText().toString()));
         all_score = all_score + rad25 - previous_cost;
         previous_cost = rad25;
@@ -123,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
         rb2.setEnabled(false);
         rb3.setEnabled(true);
         rb.setEnabled(true);
-        full_amount = findViewById(R.id.txtResult);
+        full_amount = findViewById(id.txtResult);
         int all_score = Integer.parseInt((full_amount.getText().toString()));
         all_score = all_score + rad30 - previous_cost;
         previous_cost = rad30;
@@ -134,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         rb2.setEnabled(true);
         rb3.setEnabled(false);
         rb.setEnabled(true);
-        full_amount = findViewById(R.id.txtResult);
+        full_amount = findViewById(id.txtResult);
         int all_score = Integer.parseInt((full_amount.getText().toString()));
         all_score = all_score + rad35 - previous_cost;
         previous_cost = rad35;
@@ -143,68 +147,72 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void check_box1_onClick(View view) {
-        checkBox = findViewById(R.id.checkBox);
+        checkBox = findViewById(id.checkBox);
         checkboxes_onClick(checkBox, ham, full_amount);
     }
 
     public void check_box2_onClick(View view) {
-        checkBox = findViewById(R.id.checkBox2);
+        checkBox = findViewById(id.checkBox2);
         checkboxes_onClick(checkBox, mushrooms, full_amount);
     }
 
     public void check_box3_onClick(View view) {
-        checkBox = findViewById(R.id.checkBox3);
+        checkBox = findViewById(id.checkBox3);
         checkboxes_onClick(checkBox, pepper, full_amount);
     }
 
     public void check_box4_onClick(View view) {
-        checkBox = findViewById(R.id.checkBox4);
+        checkBox = findViewById(id.checkBox4);
         checkboxes_onClick(checkBox, chicken, full_amount);
     }
 
     public void check_box5_onClick(View view) {
-        checkBox = findViewById(R.id.checkBox5);
+        checkBox = findViewById(id.checkBox5);
         checkboxes_onClick(checkBox, pineapple, full_amount);
     }
 
     public void check_box6_onClick(View view) {
-        checkBox = findViewById(R.id.checkBox6);
+        checkBox = findViewById(id.checkBox6);
         checkboxes_onClick(checkBox, cheese, full_amount);
     }
 
     public void button_order_onClick(View view) {
-        btn = findViewById(R.id.button);
-        CheckBox checkBox1 = findViewById(R.id.checkBox);
-        CheckBox checkBox2 = findViewById(R.id.checkBox2);
-        CheckBox checkBox3 = findViewById(R.id.checkBox3);
-        CheckBox checkBox4 = findViewById(R.id.checkBox4);
-        CheckBox checkBox5 = findViewById(R.id.checkBox5);
-        CheckBox checkBox6 = findViewById(R.id.checkBox6);
-        rb = findViewById(R.id.radioButton);
-        rb2 = findViewById(R.id.radioButton2);
-        rb3 = findViewById(R.id.radioButton3);
+        btn = findViewById(id.button);
+        CheckBox checkBox1 = findViewById(id.checkBox);
+        CheckBox checkBox2 = findViewById(id.checkBox2);
+        CheckBox checkBox3 = findViewById(id.checkBox3);
+        CheckBox checkBox4 = findViewById(id.checkBox4);
+        CheckBox checkBox5 = findViewById(id.checkBox5);
+        CheckBox checkBox6 = findViewById(id.checkBox6);
+        TextView textViewCardNumber = findViewById(id.cardNumber);
+        TextView textViewName = findViewById(id.input_name);
+        TextView textViewPhone = findViewById(id.input_phone);
+
+        rb = findViewById(id.radioButton);
+        rb2 = findViewById(id.radioButton2);
+        rb3 = findViewById(id.radioButton3);
         String adds = "";
         String payment;
         String radius;
         payment_kind = spinner.getSelectedItem().toString();
 
         if (checkBox1.isChecked()) {
-            adds += R.string.chb_adds_1 + " ";
+            adds += "Ветчина";
         }
         if (checkBox2.isChecked()) {
-            adds += R.string.chb_adds_2 + " ";
+            adds += "Грибы";
         }
         if (checkBox3.isChecked()) {
-            adds += R.string.chb_adds_3 + " ";
+            adds += "Перец";
         }
         if (checkBox4.isChecked()) {
-            adds += R.string.chb_adds_4 + " ";
+            adds += String.format("%d ", chb_adds_4);
         }
         if (checkBox5.isChecked()) {
-            adds += R.string.chb_adds_5 + " ";
+            adds += String.format("%d ", chb_adds_5);
         }
         if (checkBox6.isChecked()) {
-            adds += R.string.chb_adds_6 + " ";
+            adds += String.format("%d ", chb_adds_6);
         }
 
         payment = full_amount.getText().toString();
@@ -217,20 +225,37 @@ public class MainActivity extends AppCompatActivity {
             radius = "35 см";
         }
 
-        if (checkBox1.isChecked() || checkBox2.isChecked() || checkBox3.isChecked() ||
-                checkBox4.isChecked() || checkBox5.isChecked() || checkBox6.isChecked()) {
+        if ((checkBox1.isChecked() || checkBox2.isChecked() || checkBox3.isChecked() ||
+                checkBox4.isChecked() || checkBox5.isChecked() || checkBox6.isChecked())
+                && textViewCardNumber.getText().length() != 0 && textViewPhone.getText().length() != 0
+                && textViewPhone.getText().length() != 0) {
 
             Toast toast = Toast.makeText(getApplicationContext(), ("Добавки: " + adds + "\r\n" +
                     "Размер: " + radius + "\r\n" +
                     "Стоимость: " + payment + " руб." + "\r\n" +
                     "Способ оплаты: " + payment_kind), Toast.LENGTH_SHORT);
             toast.show();
+        } else {
+            String do_what_must_be_done = "";
+            if (!(checkBox1.isChecked() || checkBox2.isChecked() || checkBox3.isChecked() ||
+                    checkBox4.isChecked() || checkBox5.isChecked() || checkBox6.isChecked())) {
+                do_what_must_be_done += "Не выбраны добавки" + "\r\n";
+            }
+            if (textViewCardNumber.getText().length() == 0) {
+                do_what_must_be_done += "Не введены данные оплаты" + "\r\n";
+            }
+            if (textViewName.getText().length() == 0 || textViewPhone.getText().length()==0) {
+                do_what_must_be_done += "Не введены данные заказчика" + "\r\n";
+            }
+            Toast toast = Toast.makeText(getApplicationContext(), (do_what_must_be_done), Toast.LENGTH_SHORT);
+            toast.show();
+
         }
     }
 
     public void switch_email_onClick(View view) {
-        switchEmail = findViewById(R.id.email_switch);
-        email = findViewById(R.id.email);
+        switchEmail = findViewById(id.email_switch);
+        email = findViewById(id.email);
         if (switchEmail.isChecked()) {
             email.setVisibility(View.VISIBLE);
         } else {
@@ -260,29 +285,34 @@ public class MainActivity extends AppCompatActivity {
             if (!newValue.equals(initialValue)) {
                 switch (newValue) {
                     case "Банковская карта":
+                        cardNumber.setText(null);
                         cardNumber.setHint("Номер карты");
                         cardDate.setVisibility(View.VISIBLE);
                         cardNumber.setVisibility(View.VISIBLE);
                         cardCCV.setVisibility(View.VISIBLE);
                         break;
                     case "Оплата наличными":
+                        cardNumber.setText("0");
                         cardDate.setVisibility(View.GONE);
                         cardNumber.setVisibility(View.GONE);
                         cardCCV.setVisibility(View.GONE);
                         break;
                     case "Яндекс.Деньги":
+                        cardNumber.setText(null);
                         cardNumber.setHint("Номер кошелька Яндекс.Деньги");
                         cardDate.setVisibility(View.GONE);
                         cardNumber.setVisibility(View.VISIBLE);
                         cardCCV.setVisibility(View.GONE);
                         break;
                     case "PayPal":
+                        cardNumber.setText(null);
                         cardNumber.setHint("Номер кошелька PayPal");
                         cardDate.setVisibility(View.GONE);
                         cardNumber.setVisibility(View.VISIBLE);
                         cardCCV.setVisibility(View.GONE);
                         break;
                     default:
+                        cardNumber.setText(null);
                         cardNumber.setHint("Номер бонусной карты");
                         cardDate.setVisibility(View.GONE);
                         cardNumber.setVisibility(View.VISIBLE);
@@ -290,8 +320,6 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
                 initialValue = newValue;
-
-
             }
         }
 
